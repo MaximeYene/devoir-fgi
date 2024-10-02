5506fffc19d2c1c92422b765c016230cfc307078
MaximeYene <maximeyene@gmail.com>
2024-10-03 00:48:05 +0100

    wip

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,30 +1,13 @@
-import * as React from "react";
+/* eslint-disable react/display-name */
+// components/ui/input.tsx
+import React from 'react';
 
-import { cn } from "@/lib/utils";
-
-// Exemple d'interface Input modifiée
-export interface InputProps {
-  type?: string; // Par exemple, vous pouvez définir les types possibles
-  accept?: string; // Acceptation des types de fichiers
-  className?: string; // Pour le style
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; // Pour gérer le changement de fichier
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    // Vous pouvez ajouter d'autres props spécifiques si nécessaire
 }
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
-Input.displayName = "Input";
+export const Input: React.FC<InputProps> = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+    return <input ref={ref} {...props} />;
+});
 
-export { Input };
+// N'oubliez pas d'ajouter le forwardRef si vous utilisez des refs
